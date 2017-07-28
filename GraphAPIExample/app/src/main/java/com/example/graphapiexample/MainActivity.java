b581db98fae2c972b4bcc4f071fc7bd630ecea64
talhahasanzia <talha.zia@mujadidia.com>
2017-07-28 15:43:08 +0500

    code
    written code

---
+++ GraphAPIExample/app/src/main/java/com/example/graphapiexample/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.graphapiexample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.facebook.share.model.ShareHashtag;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+	
+	private ShareDialog shareDialog;
+	private String name, surname, imageUrl;
+	private String TAG = "MainActivity";
+	
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_main );
+		
+		Bundle inBundle = getIntent().getExtras();
+		name = inBundle.getString( "name" );
+		surname = inBundle.getString( "surname" );
+		imageUrl = inBundle.getString( "imageUrl" );
+		
+		TextView nameView = (TextView) findViewById( R.id.nameAndSurname );
+		nameView.setText( "" + name + " " + surname );
+	}
+	
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		
+	}
+	
+	@Override
+	public void onClick( View view )
+	{
+		switch ( view.getId() )
+		{
+			case R.id.share:
+				share();
+				break;
+			
+			case R.id.getPosts:
+				getPosts();
+				break;
+			
+			
+			case R.id.postMessage:
+				postOnTheWall();
+				break;
+			
+			case R.id.logout:
+				logout();
+				break;
+			
+		}
+	}
+	
+	private void logout()
+	{
+		
+		LoginManager.getInstance().logOut();
+		Intent login = new Intent( MainActivity.this, LoginActivity.class );
+		startActivity( login );
+		finish();
+	}
+	
+	private void share()
+	{
+		shareDialog = new ShareDialog( this );
+		List<String> taggedUserIds = new ArrayList<String>();
+		taggedUserIds.add( "{USER_ID}" );
+		taggedUserIds.add( "{USER_ID}" );
+		taggedUserIds.add( "{USER_ID}" );
+		
+		ShareLinkContent content = new ShareLinkContent.Builder()
+				.setContentUrl( Uri.parse( "http://www.sunnahpro.com" ) )
+				.setContentTitle( "This is a content title" )
+				.setContentDescription( "This is a description" )
+				.setShareHashtag( new ShareHashtag.Builder().setHashtag( "#sitepoint" ).build() )
+				.setPeopleIds( taggedUserIds )
+				.setPlaceId( "{PLACE_ID}" )
+				.build();
+		
+		shareDialog.show( content );
+	}
+	
+	private void getPosts()
+	{
+		
+		
+		new GraphRequest(
+				AccessToken.getCurrentAccessToken(),
+				"/me/posts",
+				null,
+				HttpMethod.GET,
+				new GraphRequest.Callback()
+				{
+					public void onCompleted( GraphResponse response )
+					{
+						Log.e( TAG, response.toString() );
+					}
+				}
+		).executeAsync();
+	}
+	
+	
+	void postOnTheWall()
+	{
+		
+		
+		Bundle params = new Bundle();
+		params.putString( "message", "This is a test message for Graph API" );
+		/* make the API call */
+		new GraphRequest(
+				AccessToken.getCurrentAccessToken(),
+				"/me/feed",
+				params,
+				HttpMethod.POST,
+				new GraphRequest.Callback()
+				{
+					public void onCompleted( GraphResponse response )
+					{
+			/* handle the result */
+					}
+				}
+		).executeAsync();
+		
+	}
+}
