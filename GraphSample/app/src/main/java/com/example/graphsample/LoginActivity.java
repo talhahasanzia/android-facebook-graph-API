a717aedfcd640d1dca0424c42088428ae5548d56
talhahasanzia <talha.zia@mujadidia.com>
2017-07-31 12:59:37 +0500

    Final code

--- GraphSample/app/src/main/java/com/example/graphsample/LoginActivity.java
+++ GraphSample/app/src/main/java/com/example/graphsample/LoginActivity.java
@@ -13,6 +13,7 @@ import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -137,8 +138,92 @@ public class LoginActivity extends AppCompatActivity
 			}
 		} );
 		
+	/* // to control log in yourself, this is a work around
+		if(loginButton.getText().equals( "Log out" ))
+			LoginManager.getInstance().logInWithReadPermissions( LoginActivity.this, Arrays.asList( "public_profile", "user_posts" ) );
+		*/
 		
-		//LoginManager.getInstance().logInWithReadPermissions( LoginActivity.this, Arrays.asList( "public_profile", "user_posts" ) );
+	
+	
+	/*LoginManager.getInstance().retrieveLoginStatus( this, new LoginStatusCallback()
+		{
+			@Override
+			public void onCompleted( AccessToken accessToken )
+			{
+				GraphRequest request = GraphRequest.newMeRequest( accessToken, new GraphRequest.GraphJSONObjectCallback()
+				{
+					@Override
+					public void onCompleted( JSONObject object, GraphResponse response )
+					{
+						Log.e( TAG, object.toString() );
+						Log.e( TAG, response.toString() );
+						
+						try
+						{
+							userId = object.getString( "id" );
+							profilePicture = new URL( "https://graph.facebook.com/" + userId + "/picture?width=500&height=500" );
+							Log.d( "PROFILE_URL", "url: " + profilePicture.toString() );
+							if ( object.has( "first_name" ) )
+							{
+								firstName = object.getString( "first_name" );
+							}
+							if ( object.has( "last_name" ) )
+							{
+								lastName = object.getString( "last_name" );
+							}
+							if ( object.has( "email" ) )
+							{
+								email = object.getString( "email" );
+							}
+							if ( object.has( "birthday" ) )
+							{
+								birthday = object.getString( "birthday" );
+							}
+							if ( object.has( "gender" ) )
+							{
+								gender = object.getString( "gender" );
+							}
+							
+							Intent main = new Intent( LoginActivity.this, MainActivity.class );
+							main.putExtra( "name", firstName );
+							main.putExtra( "surname", lastName );
+							main.putExtra( "imageUrl", profilePicture.toString() );
+							startActivity( main );
+							finish();
+						}
+						catch ( JSONException e )
+						{
+							e.printStackTrace();
+						}
+						catch ( MalformedURLException e )
+						{
+							e.printStackTrace();
+						}
+						
+					}
+				} );
+				//Here we put the requested fields to be returned from the JSONObject
+				Bundle parameters = new Bundle();
+				parameters.putString( "fields", "id, first_name, last_name, email, birthday, gender" );
+				request.setParameters( parameters );
+				request.executeAsync();
+			}
+			
+			@Override
+			public void onFailure()
+			{
+				//Toast.makeText( LoginActivity.this, "Could not log in.", Toast.LENGTH_SHORT ).show();
+			}
+			
+			@Override
+			public void onError( Exception exception )
+			{
+				
+			}
+		} );
+	*/
+		
+		LoginManager.getInstance().logInWithReadPermissions( LoginActivity.this, Arrays.asList( "public_profile", "user_posts" ) );
 		
 	}
 	
